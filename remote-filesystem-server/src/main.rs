bedea56314ce4505769ac17917cb15b23a5506e8
ludihan <65617704+ludihan@users.noreply.github.com>
2023-12-03 23:32:02 -0300

    add unfinished server

--- remote-filesystem-server/src/main.rs
+++ remote-filesystem-server/src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello, world!");
+use std::path::PathBuf;
+use std::time::UNIX_EPOCH;
+use std::env::current_dir;
+use std::fs;
+use serde::{Serialize,Deserialize};
+
+fn main() -> std::io::Result<()>{
+    let scanned_dir = DirScan::new(current_dir()?);
+    Ok(())
+}
+
+#[derive(Serialize, Deserialize)]
+struct Info {
+    name: String,
+    size: u64,
+    last_modification: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct DirScan {
+    path: String,
+    dir: Vec<Info>,
+    files: Vec<Info>,
+}
+
+impl DirScan {
+    fn new(path: PathBuf) -> DirScan {
+        let scanned_dir = DirScan {
+            path: current_dir().unwrap().display().to_string(),
+            dir: Vec::new(),
+            files: Vec::new()
+        };
+        let path = current_dir().unwrap().display();
+        let metadata = fs::metadata(path);
+    }
+
+    fn parent() -> DirScan {
+
+    }
 }
